e39f848c130b2d136bf7fda494ec162e49477520
Max Pumperla <max.pumperla@googlemail.com>
2018-02-12 22:37:06 +0100

    linting

--- graphs/rnn/primitive_lstm/__init__.py
+++ graphs/rnn/primitive_lstm/__init__.py
@@ -1,12 +1,11 @@
 import numpy as np
+from helper.load_save_utils import TensorFlowPersistor, InputDictionary
 
-from helper import load_save_utils
-
-model_name = "primitive_lstm"
-save_dir = model_name
+save_dir = "primitive_lstm"
+PERSISTOR = TensorFlowPersistor(save_dir)
 
 '''
-Sequence is just add 0.01 
+Sequence is just add 0.01
 input shape is ((-1, timesteps, num_input))
 '''
 timesteps = 7
@@ -14,32 +13,35 @@ featuresize = 2
 minibatch = 5
 
 np.random.seed(13)
-l = np.linspace(0, timesteps - 1, timesteps).reshape(1, timesteps) / 100;
+l = np.linspace(0, timesteps - 1, timesteps).reshape(1, timesteps) / 100
 timeseq = np.zeros((minibatch, featuresize, timesteps))
 timeseq += l
 r = np.random.uniform(0, 0.5, minibatch * featuresize).reshape(minibatch, featuresize, 1)
 timeseq += r
 
 
-def get_input(name):
-    np.random.seed(13)
-    if name == "input":
-        input = timeseq[:, :, :-1]
-        load_save_utils.save_input(input, "input", save_dir)
-        return input
+def get_tf_persistor():
+    return PERSISTOR
 
 
-def get_output(name):
-    if name == "output":
-        return timeseq[:, :, -1].reshape((minibatch, featuresize))
+class SimpleLstmInput(InputDictionary):
 
+    def __init__(self):
+        self.featuresize = featuresize
+        self.timesteps = timesteps
+        self.minibatch = minibatch
 
-def list_inputs():
-    return ["input"]
+    def get_input(self, name):
+        np.random.seed(13)
+        if name == "input":
+            return timeseq[:, :, :-1]
 
+    def get_output(self, name):
+        if name == "output":
+            return timeseq[:, :, -1].reshape((minibatch, featuresize))
 
-def get_inputs():
-    my_input_dict = {}
-    for a_input in list_inputs():
-        my_input_dict[a_input] = get_input(a_input)
-    return my_input_dict
+    def get_inputs(self):
+        my_input_dict = {}
+        for a_input in self.list_inputs():
+            my_input_dict[a_input] = self.get_input(a_input)
+        return my_input_dict
--- graphs/rnn/primitive_lstm/primitive_lstm_save_model.py
+++ graphs/rnn/primitive_lstm/primitive_lstm_save_model.py
@@ -1,42 +1,43 @@
 import tensorflow as tf
-from graphs.rnn.primitive_lstm import featuresize, timesteps, minibatch, get_input, get_output, save_dir
 from tensorflow.contrib import rnn
+from graphs.mlp.bias_add import SimpleLstmInput, get_tf_persistor
 
-from helper import load_save_utils
+persistor = get_tf_persistor()
+inputs = SimpleLstmInput()
 
 num_hidden = 3
 learning_rate = 0.001
-training_steps = 10
+training_steps = 10000
 display_step = 10
 
 # tf Graph input
-X = tf.placeholder("float", [None, featuresize, timesteps-1], name="input")
-Y = tf.placeholder("float", [None, featuresize])
+X = tf.placeholder("float", [None, input.featuresize, input.timesteps - 1], name="input")
+Y = tf.placeholder("float", [None, input.featuresize])
 weights = {
-    'out': tf.Variable(tf.random_normal([num_hidden, featuresize]))
+    'out': tf.Variable(tf.random_normal([num_hidden, input.featuresize]))
 }
 biases = {
-    'out': tf.Variable(tf.random_normal([featuresize]))
+    'out': tf.Variable(tf.random_normal([input.featuresize]))
 }
 
 
 def RNN(x, weights, biases):
-    x = tf.unstack(x,axis=2)
+    x = tf.unstack(x, axis=2)
     lstm_cell = rnn.BasicLSTMCell(num_hidden, forget_bias=1.0)
-    #inputs: A length T list of inputs,
+    # inputs: A length T list of inputs,
     #  each a Tensor of shape [batch_size, input_size], or a nested tuple of such elements.
     outputs, states = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
     return tf.matmul(outputs[-1], weights['out']) + biases['out']
 
 
 preout = RNN(X, weights, biases)
-output = tf.identity(preout,name="output")
+output = tf.identity(preout, name="output")
 
 # Define loss and optimizer
-loss_op = tf.reduce_sum(tf.pow(output-Y, 2))/(2*minibatch) #MSE
+loss_op = tf.reduce_sum(tf.pow(output - Y, 2)) / (2 * input.minibatch)  # MSE
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
-accuracy = tf.pow(output-Y,2)
+accuracy = tf.pow(output - Y, 2)
 
 all_saver = tf.train.Saver()
 init = tf.global_variables_initializer()
@@ -46,23 +47,22 @@ with tf.Session() as sess:
     # Run the initializer
     sess.run(init)
     for step in range(1, training_steps + 1):
-        sess.run(train_op, feed_dict={X: get_input("input"), Y: get_output("output")})
+        sess.run(train_op, feed_dict={X: input.get_input("input"), Y: input.get_output("output")})
         if step % display_step == 0 or step == 1:
             # Calculate batch loss and accuracy
-            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: get_input("input"),
-                                                                 Y: get_output("output")})
-            print("Step " + str(step) + ", Loss= " + \
+            loss, acc = sess.run([loss_op, accuracy], feed_dict={X: input.get_input("input"),
+                                                                 Y: input.get_output("output")})
+            print("Step " + str(step) + ", Loss= " +
                   "{:.4f}".format(loss))
-            #print(acc)
+            # print(acc)
     print("Optimization Finished!")
 
-    prediction = sess.run(output, feed_dict={X: get_input("input")})
-    print(get_input("input"))
-    print(get_input("input").shape)
+    prediction = sess.run(output, feed_dict={X: input.get_input("input")})
+    print(input.get_input("input"))
+    print(input.get_input("input").shape)
     print("====")
-    print(get_output("output"))
+    print(input.get_output("output"))
     print("====")
     print(prediction)
-    load_save_utils.save_prediction(save_dir, prediction)
-    load_save_utils.save_graph(sess, all_saver, save_dir)
-
+    persistor.save_prediction(prediction)
+    persistor.save_graph(sess, all_saver)
