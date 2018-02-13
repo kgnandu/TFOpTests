157401e8d0a3f8c09c5ad0c8156dda1493c3e890
Max Pumperla <max.pumperla@googlemail.com>
2018-02-13 10:17:20 +0100

    remove old bias add

--- graphs/mlp/bias_add/bias_add_freeze.py
+++
@@ -1,8 +0,0 @@
-from graphs.mlp.bias_add import BiasAddInput, get_tf_persistor
-
-persistor = get_tf_persistor()
-inputs = BiasAddInput()
-
-persistor.freeze_n_save_graph()
-persistor.write_frozen_graph_txt()
-persistor.save_intermediate_nodes(inputs())
