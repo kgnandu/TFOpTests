4dec7b0b028111ec7f02d2fc180b9b994e626c0c
eraly <susan.eraly@gmail.com>
2018-02-22 21:19:22 -0800

    added some ops

--- tests/mathops/test_tensor_dot_misc.py
+++ tests/mathops/test_tensor_dot_misc.py
@@ -36,7 +36,7 @@ class TensorDotMisc(TestGraph):
 
 
 def test_tensor_dot_misc():
-    tensor_dot_misc = TensorDotMisc(feature_size_a=[36, 3, 4, 5], feature_size_b=[5, 5, 3, 4])
+    tensor_dot_misc = TensorDotMisc(seed=713, feature_size_a=[36, 3, 4, 5], feature_size_b=[5, 5, 3, 4])
     in_node_a = tensor_dot_misc.get_placeholder("input_a")
     in_node_b = tensor_dot_misc.get_placeholder("input_b")
     tensor_dot_node = tf.tensordot(in_node_a, in_node_b, axes=[[3, 1], [1, 2]])  # 36,4,5,4
@@ -46,15 +46,6 @@ def test_tensor_dot_misc():
     space_to_depth_node = tf.round(tf.space_to_depth(batch_to_space_node_b, block_size=2))  # 1,3,16,20
     some_add = tf.add(tf.Variable(tf.random_normal((16, 20), dtype=tf.float64)), space_to_depth_node)  # broadcast
     out_node = tf.round(some_add, name="output")
-
-    '''
-    init = tf.global_variables_initializer()
-    sess = tf.Session()
-    sess.run(init)
-    pred = sess.run(out_node, feed_dict={in_node_a: tensor_dot_misc.a_data, in_node_b: tensor_dot_misc.b_data})
-    print(pred)
-    print(pred.shape)
-    '''
 
     placeholders = [in_node_a, in_node_b]
     predictions = [out_node]
