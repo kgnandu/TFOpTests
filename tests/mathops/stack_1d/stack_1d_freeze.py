b4a60fad28dce98c230c74a70169caf95b91961a
eraly <susan.eraly@gmail.com>
2018-02-25 21:13:56 -0800

    port more tests

--- tests/mathops/stack_1d/stack_1d_freeze.py
+++
@@ -1,8 +0,0 @@
-from . import  save_dir
-from tfoptests import persistor
-import numpy as np
-
-
-persistor.freeze_n_save_graph(save_dir)
-persistor.write_frozen_graph_txt(save_dir)
-persistor.save_intermediate_nodes(save_dir,{})
