e79a7d3e70b7572036098f10316b17a3d453fc23
eraly <susan.eraly@gmail.com>
2018-02-25 18:56:12 -0800

    port more tests

--- tests/mathops/simple_while/simple_while_freeze.py
+++
@@ -1,6 +0,0 @@
-from tests.mathops.simple_while import save_dir, get_inputs
-from tfoptests import persistor
-
-persistor.freeze_n_save_graph(save_dir,output_node_names='output/Exit')
-persistor.write_frozen_graph_txt(save_dir)
-persistor.save_intermediate_nodes(save_dir,get_inputs())
--- tests/mathops/test_g_03.py
+++ tests/mathops/test_g_03.py
@@ -24,7 +24,7 @@ class MathOpsThree(TestGraph):
 def test_mathops_three():
     mathops_3 = MathOpsThree(seed=19)
     in_node_0 = mathops_3.get_placeholder("input_0")
-    k0 = tf.Variable(tf.random_normal([8, 8],dtype=tf.float64), name="in0")
+    k0 = tf.Variable(tf.random_normal([8, 8], dtype=tf.float64), name="in0")
     n4 = tf.depth_to_space(in_node_0, block_size=4)
     n5 = tf.cumsum(n4, axis=-3, exclusive=True, reverse=True)
     n6 = tf.diag_part(tf.reshape(n5, [8, 8, 8, 8]))
